e4f29c02de49db53823f53f0836a7ce98d5479f3
aartiveer <aartiveer1212@gmail.com>
2025-09-20 13:35:40 +0530

    Distinguish vision and mission section properly

--- src/components/OurMission.js
+++ src/components/OurMission.js
@@ -1,16 +1,15 @@
 import React from "react";
 import './OurMission.css';
 
-const VisionSection = () => {
+const MissionSection = () => {
     return (
-        <section className="vision">
+        <section className="mission">
             <h1>Our Mission</h1>
             <p>To equip individuals and organizations with cutting-edge technology knowledge and practical skills in Embedded Linux development, customization,
-and optimization, enabling them to unleash the full potential of embedded devices and IoT solutions.
-</p>
-            
+                and optimization, enabling them to unleash the full potential of embedded devices and IoT solutions.
+            </p>
         </section>
     );
 };
 
-export default VisionSection;
+export default MissionSection;
