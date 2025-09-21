5fa6e66716c809400989f525d3c076ab6f2da1a7
aartiveer <aartiveer1212@gmail.com>
2025-09-21 20:27:10 +0530

    Improved the overall UI

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -9,31 +9,41 @@ const FooterSection = () => {
         <div className="footer-col left">
           <h2>PenguinTech</h2>
           <p>Work towards improving the products</p>
-          <div className="social-icons">
-            <i className="fab fa-youtube"></i>
-            <i className="fab fa-github"></i>
-            <i className="fab fa-linkedin"></i>
-            <i className="fab fa-google"></i>
-            <i className="fab fa-twitter"></i>
-          </div>
+          <div className="footersocialicons">
+                <a href="https://youtube.com" className="icon-button youtube" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-youtube"></i>
+                </a>
+                <a href="https://github.com/aartiveer" className="icon-button github" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/aarti-veer/" className="icon-button linkedin" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="mailto:aartiveer1212@gmail.com" className="icon-button google">
+                    <i className="fab fa-google"></i>
+                </a>
+                <a href="https://twitter.com" className="icon-button twitter" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-twitter"></i>
+                </a>
+            </div>
         </div>
 
         <div className="footer-col">
           <h3>Company</h3>
           <ul>
-            <li>Home</li>
-            <li>Our Services</li>
-            <li>Trainings</li>
-            <li>Blogs</li>
-            <li>About Us</li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/services">our services</a></li>
+            <li><a href="/trainings">trainings</a></li>
+            <li><a href="/blogs">blogs</a></li>
+            <li><a href="/about">about us</a></li>
           </ul>
         </div>
 
         <div className="footer-col">
           <h3>Get Help</h3>
           <ul>
-            <li>FAQ</li>
-            <li>Contact Us</li>
+            <li><a href="/FAQ">FAQ</a></li>
+            <li><a href="/Contact">Contact us</a></li>
           </ul>
         </div>
 
@@ -49,7 +59,7 @@ const FooterSection = () => {
       </div>
 
       <div className="footer-bottom">
-        <p>Copyright ©2024 PenguinTech | Designed by Aarti Veer</p>
+        <p>Copyright ©2025 PenguinTech | Designed by Aarti Veer</p>
       </div>
     </footer>
   );
